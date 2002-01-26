29119dead7ff12541a3f5c154d2128573f740a78
Scott Sanders <sanders@apache.org>
2002-01-26 02:47:42 +0000

    Savepoint.  Does not compile or work yet.  More work this weekend.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/io/trunk@140288 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/io/FileUtils.java
@@ -0,0 +1,300 @@
+package org.apache.commons.io;
+
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ *    "Apache Turbine" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Apache Turbine", nor may "Apache" appear in their name, without
+ *    prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+/**
+ * Common {@link java.io.File} manipulation routines.
+ *
+ * Taken from the commons-utils repo.
+ *
+ * @author <a href="mailto:sanders@apache.org">Scott Sanders</a>
+ * @author <a href="mailto:dlr@finemaltcoding.com">Daniel Rall</a>
+ * @author <a href="mailto:Christoph.Reck@dlr.de">Christoph.Reck</a>
+ * @version $Id: FileUtils.java,v 1.1 2002/01/26 02:47:42 sanders Exp $
+ */
+public class FileUtils
+{
+    /**
+     * The number of bytes in a kilobyte.
+     */
+    public static final int ONE_KB = 1024;
+
+    /**
+     * The number of bytes in a megabyte.
+     */
+    public static final int ONE_MB = ONE_KB * ONE_KB;
+
+    /**
+     * The number of bytes in a gigabyte.
+     */
+    public static final int ONE_GB = ONE_KB * ONE_MB;
+
+    /**
+     * Returns a human-readable version of the file size (original is in
+     * bytes).
+     *
+     * @param size The number of bytes.
+     * @return     A human-readable display value (includes units).
+     */
+    public static String byteCountToDisplaySize(int size)
+    {
+        String displaySize;
+
+        if (size / ONE_GB > 0)
+        {
+            displaySize = String.valueOf(size / ONE_GB) + " GB";
+        }
+        else if (size / ONE_MB > 0)
+        {
+            displaySize = String.valueOf(size / ONE_MB) + " MB";
+        }
+        else if (size / ONE_KB > 0)
+        {
+            displaySize = String.valueOf(size / ONE_KB) + " KB";
+        }
+        else
+        {
+            displaySize = String.valueOf(size) + " bytes";
+        }
+
+        return displaySize;
+    }
+
+    /**
+     * Returns the directory path portion of a file specification string.
+     * Matches the equally named unix command.
+     * @return The directory portion excluding the ending file separator.
+     */
+    public static String dirname(String filename)
+    {
+        int i = filename.lastIndexOf(File.separator);
+        return (i >= 0 ? filename.substring(0, i) : "");
+    }
+
+    /**
+     * Returns the filename portion of a file specification string.
+     * @return The filename string with extension.
+     */
+    public static String filename(String filename)
+    {
+        int i = filename.lastIndexOf(File.separator);
+        return (i >= 0 ? filename.substring(i + 1) : filename);
+    }
+
+    /**
+     * Returns the filename portion of a file specification string.
+     * Matches the equally named unix command.
+     * @return The filename string without extension.
+     */
+    public static String basename(String filename)
+    {
+        return basename(filename, extension(filename));
+    }
+
+    /**
+     * Returns the filename portion of a file specification string.
+     * Matches the equally named unix command.
+     */
+    public static String basename(String filename, String suffix)
+    {
+        int i = filename.lastIndexOf(File.separator) + 1;
+        int lastDot = ((suffix != null) && (suffix.length() > 0))
+          ? filename.lastIndexOf(suffix) : -1;
+
+        if (lastDot >= 0)
+        {
+          return filename.substring(i, lastDot);
+        }
+        else if (i > 0)
+        {
+          return filename.substring(i);
+        }
+        else
+        {
+          return filename; // else returns all (no path and no extension)
+        }
+    }
+
+    /**
+     * Returns the extension portion of a file specification string.
+     * This everything after the last dot '.' in the filename (including
+     * the dot).
+     */
+    public static String extension(String filename)
+    {
+        int lastDot = filename.lastIndexOf('.');
+
+        if (lastDot >= 0)
+        {
+          return filename.substring(lastDot);
+        }
+        else
+        {
+          return "";
+        }
+    }
+
+    /**
+     * Check if a file exits.
+     *
+     * @param fileName The name of the file to check.
+     * @return true if file exists.
+     */
+    public static boolean fileExists(String fileName)
+    {
+        File file = new File(fileName);
+        return file.exists();
+    }
+
+    /**
+     * Reads the contents of a file.
+     *
+     * @param fileName The name of the file to read.
+     * @return The file contents or null if read failed.
+     */
+    public static String fileRead(String fileName) throws Exception
+    {
+        StringBuffer buf = new StringBuffer();
+
+        FileInputStream in = new FileInputStream(fileName);
+
+        int count;
+        byte[] b = new byte[512];
+        while ( (count = in.read(b)) > 0 )  // blocking read
+        {
+            buf.append( new String(b, 0, count) );
+        }
+
+        in.close();
+
+        return buf.toString();
+    }
+
+    /**
+     * Writes data to a file. The file will be created if it does not exist.
+     *
+     * @param fileName The name of the file to write.
+     * @param data The content to write to the file.
+     */
+    public static void fileWrite(String fileName, String data) throws Exception
+    {
+        FileOutputStream out = new FileOutputStream(fileName);
+        out.write( data.getBytes() );
+        out.close();
+    }
+
+    /**
+     * Deletes a file.
+     *
+     * @param fileName The name of the file to delete.
+     */
+    public static void fileDelete(String fileName)
+    {
+        File file = new File(fileName);
+        file.delete();
+    }
+
+    /**
+     * Waits for NFS to propagate a file creation, imposing a timeout.
+     *
+     * @param fileName The name of the file.
+     * @param seconds The maximum time in seconds to wait.
+     * @return True if file exists.
+     */
+    public static boolean waitFor(String fileName, int seconds)
+    {
+        File file = new File(fileName);
+        int timeout = 0;
+        int tick = 0;
+        while ( ! file.exists() )
+        {
+            if (tick++ >= 10)
+            {
+                tick = 0;
+                if (timeout++ > seconds)
+                {
+                    return false;
+                }
+            }
+            try
+            {
+                Thread.sleep(100);
+            }
+            catch(InterruptedException ignore)
+            {
+            }
+            catch(Exception ex)
+            {
+                break;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Creates a file handle.
+     *
+     * @param fileName The name of the file.
+     * @return A <code>File</code> instance.
+     */
+    public static File getFile(String fileName)
+    {
+        return new File(fileName);
+    }
+}
---
+++ src/test/org/apache/commons/io/IOTestCase.java
@@ -0,0 +1,126 @@
+/*
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//io/src/test/org/apache/commons/io/Attic/IOTestCase.java,v 1.1 2002/01/26 02:47:42 sanders Exp $
+ * $Revision: 1.1 $
+ * $Date: 2002/01/26 02:47:42 $
+ *
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999-2002 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Commons", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.commons.io;
+
+
+import java.lang.reflect.InvocationTargetException;
+
+import junit.framework.TestCase;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+
+/**
+ * <p>
+ *  Test Case for the IO classes. </p>
+ *
+ * <p>
+ *  Template for this stolen from Craigs BeanUtils PropertyUtilsTestCase
+ * </p>
+ *
+ * @author <a href="mailto:sanders@apache.org">Scott Sanders</a>
+ * @version $Revision: 1.1 $
+ */
+
+public class IOTestCase extends TestCase {
+
+    /**
+     * Construct a new instance of this test case.
+     *
+     * @param name Name of the test case
+     */
+    public IOTestCase(String name) {
+        super(name);
+    }
+
+
+    /**
+     * Set up instance variables required by this test case.
+     */
+    public void setUp() {
+    }
+
+
+    /**
+     * Return the tests included in this test suite.
+     */
+    public static Test suite() {
+        return (new TestSuite(IOTestCase.class));
+    }
+
+    /**
+     * Tear down instance variables required by this test case.
+     */
+    public void tearDown() {
+    }
+
+
+    /**
+     *  tests the string and int arrays of TestBean
+     */
+    public void testSomething() {
+
+    }
+
+
+}
+
