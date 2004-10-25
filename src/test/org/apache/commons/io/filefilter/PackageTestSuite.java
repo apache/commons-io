1e23e4f80985edbf7ce2de16bbeb26af90eab3ec
Stephen Colebourne <scolebourne@apache.org>
2004-10-25 23:43:32 +0000

    Add package test suites
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/io/trunk@140625 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/commons/io/filefilter/PackageTestSuite.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.io.filefilter;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+/**
+ * A basic test suite that tests all the IO filefilter package.
+ * 
+ * @author Stephen Colebourne
+ */
+public class PackageTestSuite {
+
+    public static void main(String[] args) {
+        TestRunner.run(suite());
+    }
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite("IO Utilities - file filters");
+        suite.addTest(new TestSuite(AndFileFilterTestCase.class));
+        suite.addTest(new TestSuite(FileFilterTestCase.class));
+        suite.addTest(new TestSuite(OrFileFilterTestCase.class));
+        return suite;
+    }
+}
