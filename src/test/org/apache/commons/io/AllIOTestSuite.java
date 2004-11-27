a68c2dc0de5f21c07d5787242ad0154dbef7afc6
Stephen Colebourne <scolebourne@apache.org>
2004-11-27 01:21:31 +0000

    Compound test suite
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/io/trunk@140643 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/commons/io/AllIOTestSuite.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.io;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+/**
+ * A basic test suite that tests all the IO component.
+ * 
+ * @author Stephen Colebourne
+ */
+public class AllIOTestSuite {
+
+    public static void main(String[] args) {
+        TestRunner.run(suite());
+    }
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite("IO");
+        suite.addTest(PackageTestSuite.suite());
+        suite.addTest(org.apache.commons.io.filefilter.PackageTestSuite.suite());
+        suite.addTest(org.apache.commons.io.input.PackageTestSuite.suite());
+        suite.addTest(org.apache.commons.io.output.PackageTestSuite.suite());
+        return suite;
+    }
+}
