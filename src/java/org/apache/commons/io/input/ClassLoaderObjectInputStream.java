49cbd0bcc73ac7cba6f008e9040bb1cb2fe470d7
Stephen Colebourne <scolebourne@apache.org>
2005-08-27 23:16:02 +0000

    Format class to IO coding standards for first release
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/io/trunk@241817 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/io/input/ClassLoaderObjectInputStream.java
+++ src/java/org/apache/commons/io/input/ClassLoaderObjectInputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001-2004 The Apache Software Foundation.
+ * Copyright 2001-2005 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,47 +22,47 @@ import java.io.ObjectStreamClass;
 import java.io.StreamCorruptedException;
 
 /**
- * A special ObjectInputStream to handle highly transient classes hosted
- * by Avalon components that are juggling many classloaders.
+ * A special ObjectInputStream that loads a class based on a specified
+ * <code>ClassLoader</code> rather than the system default.
+ * <p>
+ * This is useful in dynamic container environments.
  *
- * @author <a href="mailto:paul_hammant@yahoo.com">Paul Hammant</a>
- * @version $Revision$ $Date$
+ * @author Paul Hammant
+ * @version $Id$
+ * @since Commons IO 1.1
  */
-public class ClassLoaderObjectInputStream
-    extends ObjectInputStream
-{
-    private ClassLoader m_classLoader;
+public class ClassLoaderObjectInputStream extends ObjectInputStream {
+
+    /** The class loader to use. */
+    private ClassLoader classLoader;
 
     /**
      * Constructs a new ClassLoaderObjectInputStream.
-     * @param classLoader ClassLoader from which classes should be loaded.
-     * @param inputStream InputStream to work on
+     *
+     * @param classLoader  the ClassLoader from which classes should be loaded
+     * @param inputStream  the InputStream to work on
      * @throws IOException in case of an I/O error
      * @throws StreamCorruptedException if the stream is corrupted
      */
-    public ClassLoaderObjectInputStream( ClassLoader classLoader,
-                                         InputStream inputStream )
-        throws IOException, StreamCorruptedException
-    {
-        super( inputStream );
-        m_classLoader = classLoader;
+    public ClassLoaderObjectInputStream(
+            ClassLoader classLoader, InputStream inputStream)
+            throws IOException, StreamCorruptedException {
+        super(inputStream);
+        this.classLoader = classLoader;
     }
 
-    /** @see java.io.ObjectInputStream#resolveClass(java.io.ObjectStreamClass) */
-    protected Class resolveClass( ObjectStreamClass objectStreamClass )
-        throws IOException, ClassNotFoundException
-    {
-        Class clazz =
-            Class.forName( objectStreamClass.getName(), false, m_classLoader );
+    /** @inheritDoc */
+    protected Class resolveClass(ObjectStreamClass objectStreamClass)
+            throws IOException, ClassNotFoundException {
+        
+        Class clazz = Class.forName(objectStreamClass.getName(), false, classLoader);
 
-        if( null != clazz )
-        {
-            return clazz; // the classloader knows of the class
-        }
-        else
-        {
+        if (clazz != null) {
+            // the classloader knows of the class
+            return clazz;
+        } else {
             // classloader knows not of class, let the super classloader do it
-            return super.resolveClass( objectStreamClass );
+            return super.resolveClass(objectStreamClass);
         }
     }
 }
