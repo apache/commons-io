9ab36a5006519acbf20627e7c81e4b507b198a11
Stephen Colebourne <scolebourne@apache.org>
2006-07-23 13:58:32 +0000

    Add FileFileFilter that accepts files and not directories
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/io/trunk@424742 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/io/filefilter/FileFileFilter.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.io.filefilter;
+
+import java.io.File;
+
+/**
+ * This filter accepts <code>File</code>s that are files (not directories).
+ * <p>
+ * For example, here is how to print out a list of the real files
+ * within the current directory:
+ *
+ * <pre>
+ * File dir = new File(".");
+ * String[] files = dir.list( FileFileFilter.FILE );
+ * for ( int i = 0; i &lt; files.length; i++ ) {
+ *     System.out.println(files[i]);
+ * }
+ * </pre>
+ *
+ * @since Commons IO 1.3
+ * @version $Revision: 155419 $ $Date: 2005-02-26 13:02:41 +0000 (Sat, 26 Feb 2005) $
+ */
+public class FileFileFilter extends AbstractFileFilter {
+
+    /** Singleton instance of file filter */
+    public static final IOFileFilter FILE = new FileFileFilter();
+
+    /**
+     * Restrictive consructor.
+     */
+    protected FileFileFilter() {
+    }
+
+    /**
+     * Checks to see if the file is a file.
+     *
+     * @param file  the File to check
+     * @return true if the file is a file
+     */
+    public boolean accept(File file) {
+        return file.isFile();
+    }
+
+}
