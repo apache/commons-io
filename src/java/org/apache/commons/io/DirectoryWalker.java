8fc87cae153493e041856c83d253d9f418dc7b15
Stephen Colebourne <scolebourne@apache.org>
2006-07-23 14:57:17 +0000

    Create DirectoryWalker by extracting from FileFinder
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/io/trunk@424750 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/io/DirectoryWalker.java
@@ -0,0 +1,219 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.io;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.filefilter.IOFileFilter;
+
+/**
+ * Abstract class that walks through a directory hierarchy and provides
+ * subclasses with convenient hooks to add specific behaviour.
+ * <p>
+ * This class operates with a {@link IOFileFilter file filter} and maximum
+ * depth to limit the files and direcories visited. Commons IO supplies many
+ * common filter implementations in the <code>filefilter</code> package.
+ * <p>
+ * This class is extended within Commons IO to provide a file finder facility.
+ * However there are many other possible extensions. For example, to delete all
+ * files and '.svn' directories, and return a list of deleted files:
+ * <pre>
+ *  public class FileCleaner extends DirectoryWalker {
+ *
+ *    public List clean(File startDirectory) {
+ *      return walk(startDirectory);
+ *    }
+ *
+ *    protected boolean handleDirectoryStart(File directory, int depth, List results) {
+ *      // skip contents of svn directories
+ *      return !".svn".equals(directory.getName());
+ *    }
+ *
+ *    protected void handleDirectoryEnd(File directory, int depth, List results) {
+ *      // delete svn directories
+ *      if (".svn".equals(directory.getName())) {
+ *        directory.delete();
+ *      }
+ *    }
+ *
+ *    protected void handleFile(File file, int depth, List results) {
+ *      // delete file and add to list of deleted
+ *      file.delete();
+ *      results.add(file);
+ *    }
+ *  }
+ * </pre>
+ *
+ * @since Commons IO 1.3
+ * @version $Revision: 424748 $
+ */
+public abstract class DirectoryWalker {
+
+    /**
+     * The file filter to use to filter files and directories.
+     */
+    private final FileFilter filter;
+    /**
+     * The limit on the directory depth to walk.
+     */
+    private final int depthLimit;
+
+    /**
+     * Construct an instance with a filter and limit the <i>depth</i> navigated to.
+     *
+     * @param filter  the filter to limit the navigation/results, may be null
+     * @param depthLimit  controls how <i>deep</i> the hierarchy is
+     *  navigated to (less than 0 means unlimited)
+     */
+    protected DirectoryWalker(FileFilter filter, int depthLimit) {
+        this.filter = filter;
+        this.depthLimit = depthLimit;
+    }
+
+    //-----------------------------------------------------------------------
+    /**
+     * Internal method that walks the directory hierarchy.
+     * <p>
+     * Most users of this class do not need to call this method. This method will
+     * be called automatically by another (public) method on the specific subclass.
+     * <p>
+     * Writers of subclasses should call this method to start the directory walk.
+     * Once called, this method will emit events as it walks the hierarchy.
+     * The event methods have the prefix <code>handle</code>.
+     *
+     * @param startDirectory  the directory to start from
+     * @return the collection of result objects, typically File objects
+     */
+    protected List walk(File startDirectory) {
+        List results = new ArrayList();
+        handleStart(startDirectory, results);
+        walk(startDirectory, 0, results);
+        handleEnd(results);
+        return results;
+    }
+
+    /**
+     * Main recursive method to examine the directory hierarchy.
+     *
+     * @param directory  the directory to examine
+     * @param depth  the directory level (starting directory = 0)
+     * @return the collection of result objects
+     */
+    private void walk(File directory, int depth, List results) {
+        boolean process = handleDirectoryStart(directory, depth, results);
+        if (process) {
+            int childDepth = depth + 1;
+            if (depthLimit < 0 || childDepth <= depthLimit) {
+                File[] files = (filter == null ? directory.listFiles() : directory.listFiles(filter));
+                if (files == null) {
+                    handleRestricted(directory);
+                } else {
+                    for (int i = 0; i < files.length; i++) {
+                        if (files[i].isDirectory()) {
+                            walk(files[i], childDepth, results);
+                        } else {
+                            handleFile(files[i], childDepth, results);
+                        }
+                    }
+                }
+            }
+        }
+        handleDirectoryEnd(directory, depth, results);
+    }
+
+    //-----------------------------------------------------------------------
+    /**
+     * Overridable callback method invoked at the start of processing.
+     * <p>
+     * This implementation does nothing.
+     *
+     * @param startDirectory  the directory to start from
+     * @param results  the collection of result objects, may be updated
+     */
+    protected void handleStart(File startDirectory, List results) {
+        // do nothing - overridable by subclass
+    }
+
+    /**
+     * Overridable callback method invoked at the start of processing each directory.
+     * <p>
+     * This method returns a boolean to indicate if the directory should be examined
+     * or not. If you return false, the next event received will be the
+     * {@link #handleDirectoryEnd} for that directory. Note that this functionality
+     * is in addition to the filtering by file filter.
+     * <p>
+     * This implementation does nothing and returns true.
+     *
+     * @param directory  the current directory being processed
+     * @param depth  the current directory level (starting directory = 0)
+     * @param results  the collection of result objects, may be updated
+     * @return true to process this directory, false to skip this directory
+     */
+    protected boolean handleDirectoryStart(File directory, int depth, List results) {
+        // do nothing - overridable by subclass
+        return true;
+    }
+
+    /**
+     * Overridable callback method invoked for each (non-directory) file.
+     * <p>
+     * This implementation does nothing.
+     *
+     * @param file  the current file being processed
+     * @param depth  the current directory level (starting directory = 0)
+     * @param results  the collection of result objects, may be updated
+     */
+    protected void handleFile(File file, int depth, List results) {
+        // do nothing - overridable by subclass
+    }
+
+    /**
+     * Overridable callback method invoked for each restricted directory.
+     *
+     * @param directory  the restricted directory
+     */
+    protected void handleRestricted(File directory) {
+        // do nothing - overridable by subclass
+    }
+
+    /**
+     * Overridable callback method invoked at the end of processing each directory.
+     * <p>
+     * This implementation does nothing.
+     *
+     * @param directory The directory being processed
+     * @param depth The directory level (starting directory = 0)
+     * @param results The collection of files found.
+     */
+    protected void handleDirectoryEnd(File directory, int depth, List results) {
+        // do nothing - overridable by subclass
+    }
+
+    /**
+     * Overridable callback method invoked at the end of processing.
+     * <p>
+     * This implementation does nothing.
+     *
+     * @param results  the collection of result objects, may be updated
+     */
+    protected void handleEnd(List results) {
+        // do nothing - overridable by subclass
+    }
+
+}
