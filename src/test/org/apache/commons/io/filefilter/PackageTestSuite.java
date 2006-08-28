7c36e3ea85001659dc4ebaaecc1d5cee5d8ba298
Stephen Colebourne <scolebourne@apache.org>
2006-08-28 11:57:00 +0000

    Apply single license layout to all java files
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/io/trunk@437680 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/io/filefilter/PackageTestSuite.java
+++ src/test/org/apache/commons/io/filefilter/PackageTestSuite.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.commons.io.filefilter;
 
 import junit.framework.Test;
