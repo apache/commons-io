166fc85e84adc7b6d390563f922a37aff595a121
Niall Pemberton <niallp@apache.org>
2006-10-09 01:12:43 +0000

    IO-95 Add NullWriter implementation
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/io/trunk@454247 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/io/output/NullWriter.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.io.output;
+
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * This {@link Writer} writes all data to the famous <b>/dev/null</b>.
+ * <p>
+ * This {@link Writer} has no destination (file/socket etc.) and all
+ * characters written to it are ignored and lost.
+ * 
+ * @version $Id$
+ */
+public class NullWriter extends Writer {
+
+    /**
+     * Constructs a new NullWriter.
+     */
+    public NullWriter() {
+    }
+
+    /** @see java.io.Writer#write(int) */
+    public void write(int idx) throws IOException {
+        //to /dev/null
+    }
+
+    /** @see java.io.Writer#write(char[]) */
+    public void write(char[] chr) throws IOException {
+        //to /dev/null
+    }
+
+    /** @see java.io.Writer#write(char[], int, int) */
+    public void write(char[] chr, int st, int end) throws IOException {
+        //to /dev/null
+    }
+
+    /** @see java.io.Writer#write(String) */
+    public void write(String str) throws IOException {
+        //to /dev/null
+    }
+
+    /** @see java.io.Writer#write(String, int, int) */
+    public void write(String str, int st, int end) throws IOException {
+        //to /dev/null
+    }
+
+    /** @see java.io.Writer#flush() */
+    public void flush() throws IOException {
+        //to /dev/null
+    }
+
+    /** @see java.io.Writer#close() */
+    public void close() throws IOException {
+        //to /dev/null
+    }
+
+}
