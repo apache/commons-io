9fb6cdda1630b2cc510c8c940d2969bbdd80de9f
Niall Kegan Pemberton <niallp@apache.org>
2010-09-29 03:22:56 +0000

    IO-198 FileFilterUtils - add ability to apply file filters to collections and arrays - thanks to Michael Wooten for the patch
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/io/trunk@1002454 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/io/filefilter/FileFilterUtils.java
+++ src/java/org/apache/commons/io/filefilter/FileFilterUtils.java
@@ -20,8 +20,12 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.io.IOCase;
 
@@ -47,6 +51,243 @@ public class FileFilterUtils {
     }
 
     //-----------------------------------------------------------------------
+
+    /**
+     * <p>
+     * Applies an {@link IOFileFilter} to the provided {@link File} 
+     * objects. The resulting array is a subset of the original file list that 
+     * matches the provided filter.
+     * </p>
+     * 
+     * <p>
+     * The {@link Set} returned by this method is not guaranteed to be thread safe.
+     * </p>
+     * 
+     * <pre>
+     * Set&lt;File&gt; allFiles = ...
+     * Set&lt;File&gt; javaFiles = FileFilterUtils.filterSet(allFiles,
+     *     FileFilterUtils.suffixFileFilter(".java"));
+     * </pre>
+     * @param filter the filter to apply to the set of files.
+     * @param files the array of files to apply the filter to.
+     * 
+     * @return a subset of <code>files</code> that is accepted by the 
+     *         file filter.
+     * @throws IllegalArgumentException if the filter is <code>null</code> 
+     *         or <code>files</code> contains a <code>null</code> value. 
+     * 
+     * @since Commons IO 2.0
+     */
+    public static File[] filter(IOFileFilter filter, File... files) {
+        if (filter == null) {
+            throw new IllegalArgumentException("file filter is null");
+        }
+        if (files == null) {
+            return new File[0];
+        }
+        List<File> acceptedFiles = new ArrayList<File>();
+        for (File file : files) {
+            if (file == null) {
+                throw new IllegalArgumentException("file array contains null");
+            }
+            if (filter.accept(file)) {
+                acceptedFiles.add(file);
+            }
+        }
+        return acceptedFiles.toArray(new File[acceptedFiles.size()]);
+    }
+
+    /**
+     * <p>
+     * Applies an {@link IOFileFilter} to the provided {@link File} 
+     * objects. The resulting array is a subset of the original file list that 
+     * matches the provided filter.
+     * </p>
+     * 
+     * <p>
+     * The {@link Set} returned by this method is not guaranteed to be thread safe.
+     * </p>
+     * 
+     * <pre>
+     * Set&lt;File&gt; allFiles = ...
+     * Set&lt;File&gt; javaFiles = FileFilterUtils.filterSet(allFiles,
+     *     FileFilterUtils.suffixFileFilter(".java"));
+     * </pre>
+     * @param filter the filter to apply to the set of files.
+     * @param files the array of files to apply the filter to.
+     * 
+     * @return a subset of <code>files</code> that is accepted by the 
+     *         file filter.
+     * @throws IllegalArgumentException if the filter is <code>null</code> 
+     *         or <code>files</code> contains a <code>null</code> value. 
+     * 
+     * @since Commons IO 2.0
+     */
+    public static File[] filter(IOFileFilter filter, Iterable<File> files) {
+        List<File> acceptedFiles = filterList(filter, files);
+        return acceptedFiles.toArray(new File[acceptedFiles.size()]);
+    }
+
+    /**
+     * <p>
+     * Applies an {@link IOFileFilter} to the provided {@link File} 
+     * objects. The resulting list is a subset of the original files that 
+     * matches the provided filter.
+     * </p>
+     * 
+     * <p>
+     * The {@link List} returned by this method is not guaranteed to be thread safe.
+     * </p>
+     * 
+     * <pre>
+     * List&lt;File&gt; filesAndDirectories = ...
+     * List&lt;File&gt; directories = FileFilterUtils.filterList(filesAndDirectories,
+     *     FileFilterUtils.directoryFileFilter());
+     * </pre>
+     * @param filter the filter to apply to each files in the list.
+     * @param files the collection of files to apply the filter to.
+     * 
+     * @return a subset of <code>files</code> that is accepted by the 
+     *         file filter.
+     * @throws IllegalArgumentException if the filter is <code>null</code> 
+     *         or <code>files</code> contains a <code>null</code> value. 
+     * @since Commons IO 2.0
+     */
+    public static List<File> filterList(IOFileFilter filter, Iterable<File> files) {
+        return filter(filter, files, new ArrayList<File>());
+    }
+
+    /**
+     * <p>
+     * Applies an {@link IOFileFilter} to the provided {@link File} 
+     * objects. The resulting list is a subset of the original files that 
+     * matches the provided filter.
+     * </p>
+     * 
+     * <p>
+     * The {@link List} returned by this method is not guaranteed to be thread safe.
+     * </p>
+     * 
+     * <pre>
+     * List&lt;File&gt; filesAndDirectories = ...
+     * List&lt;File&gt; directories = FileFilterUtils.filterList(filesAndDirectories,
+     *     FileFilterUtils.directoryFileFilter());
+     * </pre>
+     * @param filter the filter to apply to each files in the list.
+     * @param files the collection of files to apply the filter to.
+     * 
+     * @return a subset of <code>files</code> that is accepted by the 
+     *         file filter.
+     * @throws IllegalArgumentException if the filter is <code>null</code> 
+     *         or <code>files</code> contains a <code>null</code> value. 
+     * @since Commons IO 2.0
+     */
+    public static List<File> filterList(IOFileFilter filter, File... files) {
+        File[] acceptedFiles = filter(filter, files);
+        return Arrays.asList(acceptedFiles);
+    }
+
+    /**
+     * <p>
+     * Applies an {@link IOFileFilter} to the provided {@link File} 
+     * objects. The resulting set is a subset of the original file list that 
+     * matches the provided filter.
+     * </p>
+     * 
+     * <p>
+     * The {@link Set} returned by this method is not guaranteed to be thread safe.
+     * </p>
+     * 
+     * <pre>
+     * Set&lt;File&gt; allFiles = ...
+     * Set&lt;File&gt; javaFiles = FileFilterUtils.filterSet(allFiles,
+     *     FileFilterUtils.suffixFileFilter(".java"));
+     * </pre>
+     * @param filter the filter to apply to the set of files.
+     * @param files the collection of files to apply the filter to.
+     * 
+     * @return a subset of <code>files</code> that is accepted by the 
+     *         file filter.
+     * @throws IllegalArgumentException if the filter is <code>null</code> 
+     *         or <code>files</code> contains a <code>null</code> value. 
+     * 
+     * @since Commons IO 2.0
+     */
+    public static Set<File> filterSet(IOFileFilter filter, File... files) {
+        File[] acceptedFiles = filter(filter, files);
+        return new HashSet<File>(Arrays.asList(acceptedFiles));
+    }
+
+    /**
+     * <p>
+     * Applies an {@link IOFileFilter} to the provided {@link File} 
+     * objects. The resulting set is a subset of the original file list that 
+     * matches the provided filter.
+     * </p>
+     * 
+     * <p>
+     * The {@link Set} returned by this method is not guaranteed to be thread safe.
+     * </p>
+     * 
+     * <pre>
+     * Set&lt;File&gt; allFiles = ...
+     * Set&lt;File&gt; javaFiles = FileFilterUtils.filterSet(allFiles,
+     *     FileFilterUtils.suffixFileFilter(".java"));
+     * </pre>
+     * @param filter the filter to apply to the set of files.
+     * @param files the collection of files to apply the filter to.
+     * 
+     * @return a subset of <code>files</code> that is accepted by the 
+     *         file filter.
+     * @throws IllegalArgumentException if the filter is <code>null</code> 
+     *         or <code>files</code> contains a <code>null</code> value. 
+     * 
+     * @since Commons IO 2.0
+     */
+    public static Set<File> filterSet(IOFileFilter filter, Iterable<File> files) {
+        return filter(filter, files, new HashSet<File>());
+    }
+
+    /**
+     * <p>
+     * Applies an {@link IOFileFilter} to the provided {@link File} 
+     * objects and appends the accepted files to the other supplied collection. 
+     * </p>
+     * 
+     * <pre>
+     * List&lt;File&gt; files = ...
+     * List&lt;File&gt; directories = FileFilterUtils.filterList(files,
+     *     FileFilterUtils.sizeFileFilter(FileUtils.FIFTY_MB), 
+     *         new ArrayList&lt;File&gt;());
+     * </pre>
+     * @param filter the filter to apply to the collection of files.
+     * @param files the collection of files to apply the filter to.
+     * @param acceptedFiles the list of files to add accepted files to.
+     * 
+     * @param <T> the type of the file collection.
+     * @return a subset of <code>files</code> that is accepted by the 
+     *         file filter.
+     * @throws IllegalArgumentException if the filter is <code>null</code> 
+     *         or <code>files</code> contains a <code>null</code> value. 
+     */
+    private static <T extends Collection<File>> T filter(IOFileFilter filter,
+            Iterable<File> files, T acceptedFiles) {
+        if (filter == null) {
+            throw new IllegalArgumentException("file filter is null");
+        }
+        if (files != null) {
+            for (File file : files) {
+                if (file == null) {
+                    throw new IllegalArgumentException("file collection contains null");
+                }
+                if (filter.accept(file)) {
+                    acceptedFiles.add(file);
+                }
+            }
+        }
+        return acceptedFiles;
+    }
+
     /**
      * Returns a filter that returns true if the filename starts with the specified text.
      * 
