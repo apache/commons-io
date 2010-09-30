2b5f9b4b7e8e5d5c5c1eac1db2113175c2d781aa
Sebastian Bazley <sebb@apache.org>
2010-09-30 01:33:38 +0000

    Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/io/trunk@1002921 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/io/input/TailerListener.java
+++ src/java/org/apache/commons/io/input/TailerListener.java
@@ -33,6 +33,8 @@ public interface TailerListener {
 
     /**
      * This method is called if the tailed file is not found.
+     * <p>
+     * <b>Note:</b> this is called from the tailer thread.
      */
     public void fileNotFound();
 
@@ -41,17 +43,23 @@ public interface TailerListener {
      *
      * This method is called before the file is reopened, and fileNotFound may
      * be called if the new file has not yet been created.
+     * <p>
+     * <b>Note:</b> this is called from the tailer thread.
      */
     public void fileRotated();
 
     /**
      * Handles a line from a Tailer.
+     * <p>
+     * <b>Note:</b> this is called from the tailer thread.
      * @param line the line.
      */
     public void handle(String line);
 
     /**
      * Handles an Exception .
+     * <p>
+     * <b>Note:</b> this is called from the tailer thread.
      * @param ex the exception.
      */
     public void handle(Exception ex);
