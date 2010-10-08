26e295fcbe53496e0cf2f60d56bacab5cbcd87de
Niall Pemberton <niallp@apache.org>
2010-10-08 21:20:58 +0000

    Refactor tests to share common code
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/io/trunk@1006027 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/io/monitor/FileSystemMonitorTestCase.java
+++ src/test/java/org/apache/commons/io/monitor/FileSystemMonitorTestCase.java
@@ -20,25 +20,11 @@ import java.io.File;
 import java.util.Collection;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.FileFilterUtils;
-import org.apache.commons.io.filefilter.HiddenFileFilter;
-import org.apache.commons.io.filefilter.IOFileFilter;
-
-import junit.framework.TestCase;
 
 /**
  * {@link FilesystemMonitor} Test Case.
  */
-public class FileSystemMonitorTestCase extends TestCase {
-
-    /** Filesystem observer */
-    protected FilesystemObserver observer;
-
-    /** Listener which collects file changes */
-    protected CollectionFilesystemListener listener = new CollectionFilesystemListener(false);
-
-    /** Directory for test files */
-    protected File testDir;
+public class FileSystemMonitorTestCase extends FilesystemObserverTestCase {
 
     /**
      * Construct a new test case.
@@ -47,35 +33,7 @@ public class FileSystemMonitorTestCase extends TestCase {
      */
     public FileSystemMonitorTestCase(String name) {
         super(name);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        testDir = new File(new File("."), "test-observer");
-        if (testDir.exists()) {
-            FileUtils.cleanDirectory(testDir);
-        } else {
-            testDir.mkdir();
-        }
-
-        IOFileFilter files = FileFilterUtils.fileFileFilter();
-        IOFileFilter javaSuffix = FileFilterUtils.suffixFileFilter(".java");
-        IOFileFilter fileFilter = FileFilterUtils.and(files, javaSuffix);
-        
-        IOFileFilter directories = FileFilterUtils.directoryFileFilter();
-        IOFileFilter visible = HiddenFileFilter.VISIBLE;
-        IOFileFilter dirFilter = FileFilterUtils.and(directories, visible);
-
-        IOFileFilter filter = FileFilterUtils.or(dirFilter, fileFilter);
-
-        // Create the observer
-        observer = new FilesystemObserver(testDir, filter);
-        observer.addListener(listener);
-        try {
-            observer.initialize();
-        } catch (Exception e) {
-            fail("Observer init() threw " + e);
-        }
+        testDirName = "test-monitor";
     }
 
     @Override
@@ -122,72 +80,13 @@ public class FileSystemMonitorTestCase extends TestCase {
     /**
      * Check all the File Collections have the expected sizes.
      */
-    private void checkFile(String label, File file, long interval, Collection<File> files) throws Exception {
+    private void checkFile(String label, File file, long interval, Collection<File> files) {
         for (int i = 0; i < 5; i++) {
-            Thread.sleep(interval);
             if (files.contains(file)) {
                 return; // found, test passes
             }
+            sleepHandleInterruped(interval);
         }
         fail(label + " " + file + " not found");
-    }
-
-    /**
-     * Check all the Collections are empty
-     */
-    private void checkCollectionsEmpty(String label) {
-        checkDirectoryCounts("EMPTY-" + label, 0, 0, 0);
-        checkFileCounts("EMPTY-" + label, 0, 0, 0);
-    }
-
-    /**
-     * Check all the Directory Collections have the expected sizes.
-     */
-    private void checkDirectoryCounts(String label, int dirCreate, int dirChange, int dirDelete) {
-        label = label + "[" + listener.getCreatedDirectories().size() +
-                        " " + listener.getChangedDirectories().size() +
-                        " " + listener.getDeletedDirectories().size() + "]";
-        assertEquals(label + ": No. of directories created",  dirCreate,  listener.getCreatedDirectories().size());
-        assertEquals(label + ": No. of directories changed",  dirChange,  listener.getChangedDirectories().size());
-        assertEquals(label + ": No. of directories deleted",  dirDelete,  listener.getDeletedDirectories().size());
-    }
-
-    /**
-     * Check all the File Collections have the expected sizes.
-     */
-    private void checkFileCounts(String label, int fileCreate, int fileChange, int fileDelete) {
-        label = label + "[" + listener.getCreatedFiles().size() +
-                        " " + listener.getChangedFiles().size() +
-                        " " + listener.getDeletedFiles().size() + "]";
-        assertEquals(label + ": No. of files created", fileCreate, listener.getCreatedFiles().size());
-        assertEquals(label + ": No. of files changed", fileChange, listener.getChangedFiles().size());
-        assertEquals(label + ": No. of files deleted", fileDelete, listener.getDeletedFiles().size());
-    }
-
-    /**
-     * Either creates a file if it doesn't exist or updates the last modified date/time
-     * if it does.
-     *
-     * @param file The file to touch
-     * @return The file
-     */
-    private File touch(File file) {
-        long lastModified = file.exists() ? file.lastModified() : 0;
-        try {
-            FileUtils.touch(file);
-            file = new File(file.getParent(), file.getName());
-            while (lastModified == file.lastModified()) {
-                try {
-                    Thread.sleep(200);
-                } catch(InterruptedException ie) {
-                    // ignore
-                }
-                FileUtils.touch(file);
-                file = new File(file.getParent(), file.getName());
-            }
-        } catch (Exception e) {
-            fail("Touching " + file +": " + e);
-        }
-        return file;
     }
 }
