464bb1796d2f95fab4e4a75ceedd5ceab872cbcd
Niall Pemberton <niallp@apache.org>
2010-10-15 01:31:41 +0000

    Fix test failing with Unix file names
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/io/trunk@1022806 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/io/monitor/FileAlterationObserverTestCase.java
+++ src/test/java/org/apache/commons/io/monitor/FileAlterationObserverTestCase.java
@@ -77,10 +77,12 @@ public class FileAlterationObserverTestCase extends AbstractMonitorTestCase {
         FileAlterationObserver observer = null;
 
         observer = new FileAlterationObserver(file);
-        assertEquals("FileAlterationObserver[file='\\foo', listeners=0]", observer.toString());
+        assertEquals("FileAlterationObserver[file='" + file.getPath() +  "', listeners=0]",
+                observer.toString());
   
         observer = new FileAlterationObserver(file, CanReadFileFilter.CAN_READ);
-        assertEquals("FileAlterationObserver[file='\\foo', CanReadFileFilter, listeners=0]", observer.toString());
+        assertEquals("FileAlterationObserver[file='" + file.getPath() +  "', CanReadFileFilter, listeners=0]",
+                observer.toString());
 
         assertEquals(file, observer.getDirectory());
   }
