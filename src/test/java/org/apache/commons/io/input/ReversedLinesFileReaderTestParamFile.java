b0bd7898b1c25f3246e18e3357e9fed8ce6e8661
Niall Pemberton <niallp@apache.org>
2012-01-07 01:49:45 +0000

    Checkstyle - replace tab characters with spaces
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/io/trunk@1228548 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/io/input/ReversedLinesFileReaderTestParamFile.java
+++ src/test/java/org/apache/commons/io/input/ReversedLinesFileReaderTestParamFile.java
@@ -45,22 +45,22 @@ public class ReversedLinesFileReaderTestParamFile {
     @Parameters
     public static Collection<Object[]> blockSizes() {
             return Arrays.asList(new Object[][] {
-            		{"test-file-20byteslength.bin", "ISO_8859_1", null},
-            		{"test-file-iso8859-1-shortlines-win-linebr.bin", "ISO_8859_1", null},
-            		{"test-file-iso8859-1.bin", "ISO_8859_1", null},
-            		{"test-file-shiftjis.bin", "Shift_JIS", null},
-            		{"test-file-utf16be.bin", "UTF-16BE", null},
-            		{"test-file-utf16le.bin", "UTF-16LE", null},
-            		{"test-file-utf8-cr-only.bin", "UTF-8", null},
-            		{"test-file-utf8-win-linebr.bin", "UTF-8", null},
+                    {"test-file-20byteslength.bin", "ISO_8859_1", null},
+                    {"test-file-iso8859-1-shortlines-win-linebr.bin", "ISO_8859_1", null},
+                    {"test-file-iso8859-1.bin", "ISO_8859_1", null},
+                    {"test-file-shiftjis.bin", "Shift_JIS", null},
+                    {"test-file-utf16be.bin", "UTF-16BE", null},
+                    {"test-file-utf16le.bin", "UTF-16LE", null},
+                    {"test-file-utf8-cr-only.bin", "UTF-8", null},
+                    {"test-file-utf8-win-linebr.bin", "UTF-8", null},
                     {"test-file-utf8-win-linebr.bin", "UTF-8", Integer.valueOf(1)},
                     {"test-file-utf8-win-linebr.bin", "UTF-8", Integer.valueOf(2)},
                     {"test-file-utf8-win-linebr.bin", "UTF-8", Integer.valueOf(3)},
                     {"test-file-utf8-win-linebr.bin", "UTF-8", Integer.valueOf(4)},
-            		{"test-file-utf8.bin", "UTF-8", null},
+                    {"test-file-utf8.bin", "UTF-8", null},
             });
     }
-	
+
     private ReversedLinesFileReader reversedLinesFileReader;
     private BufferedReader bufferedReader;
 
@@ -69,48 +69,48 @@ public class ReversedLinesFileReaderTestParamFile {
     private final int buffSize;
 
     public ReversedLinesFileReaderTestParamFile(String fileName, String encoding, Integer buffsize) {
-    	this.fileName = fileName;
-    	this.encoding = encoding;
-    	this.buffSize = buffsize == null ? 4096 : buffsize.intValue();
+        this.fileName = fileName;
+        this.encoding = encoding;
+        this.buffSize = buffsize == null ? 4096 : buffsize.intValue();
     }
 
-	@Test
-	public void testDataIntegrityWithBufferedReader() throws URISyntaxException, IOException {
-		File testFileIso = new File(this.getClass().getResource("/"+fileName).toURI());
-		reversedLinesFileReader = new ReversedLinesFileReader(testFileIso, buffSize, encoding);
-		
-		Stack<String> lineStack = new Stack<String>();
-		
-		bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(testFileIso), encoding));
-		String line = null;
-		
-		// read all lines in normal order
-		while((line = bufferedReader.readLine())!=null) {
-			lineStack.push(line);
-		}
-		
-		// read in reverse order and compare with lines from stack
-		while((line = reversedLinesFileReader.readLine())!=null) {
-			String lineFromBufferedReader = lineStack.pop();
-			assertEquals(lineFromBufferedReader, line);
-		}		
-		
-	}	
+    @Test
+    public void testDataIntegrityWithBufferedReader() throws URISyntaxException, IOException {
+        File testFileIso = new File(this.getClass().getResource("/"+fileName).toURI());
+        reversedLinesFileReader = new ReversedLinesFileReader(testFileIso, buffSize, encoding);
 
-	@After
-	public void closeReader() {
-		try {
-			bufferedReader.close();
-		} catch(Exception e) {
-			// ignore
-		}		
-		try {
-			reversedLinesFileReader.close();
-		} catch(Exception e) {
-			// ignore
-		}
-	}
-	
+        Stack<String> lineStack = new Stack<String>();
+
+        bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(testFileIso), encoding));
+        String line = null;
+
+        // read all lines in normal order
+        while((line = bufferedReader.readLine())!=null) {
+            lineStack.push(line);
+        }
+
+        // read in reverse order and compare with lines from stack
+        while((line = reversedLinesFileReader.readLine())!=null) {
+            String lineFromBufferedReader = lineStack.pop();
+            assertEquals(lineFromBufferedReader, line);
+        }
+
+    }
+
+    @After
+    public void closeReader() {
+        try {
+            bufferedReader.close();
+        } catch(Exception e) {
+            // ignore
+        }
+        try {
+            reversedLinesFileReader.close();
+        } catch(Exception e) {
+            // ignore
+        }
+    }
+
 
 
 }
