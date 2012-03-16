94e64536b14fb6d0726b21af9358a1e0a890864a
Sebastian Bazley <sebb@apache.org>
2012-03-16 10:57:15 +0000

    Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/io/trunk@1301432 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/io/IOUtils.java
+++ src/main/java/org/apache/commons/io/IOUtils.java
@@ -115,7 +115,7 @@ public class IOUtils {
     }
 
     /**
-     * The default buffer size to use for 
+     * The default buffer size ({@value}) to use for 
      * {@link #copyLarge(InputStream, OutputStream)}
      * and
      * {@link #copyLarge(Reader, Writer)}
@@ -1435,6 +1435,8 @@ public class IOUtils {
      * <p>
      * This method buffers the input internally, so there is no need to use a
      * <code>BufferedInputStream</code>.
+     * <p>
+     * The buffer size is given by {@link #DEFAULT_BUFFER_SIZE}.
      * 
      * @param input  the <code>InputStream</code> to read from
      * @param output  the <code>OutputStream</code> to write to
@@ -1538,6 +1540,8 @@ public class IOUtils {
      * <p>
      * This method buffers the input internally, so there is no need to use a
      * <code>BufferedReader</code>.
+     * <p>
+     * The buffer size is given by {@link #DEFAULT_BUFFER_SIZE}.
      *
      * @param input  the <code>Reader</code> to read from
      * @param output  the <code>Writer</code> to write to
