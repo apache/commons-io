aad956b558052102cc9335144c07e4488d374afb
Gary D. Gregory <ggregory@apache.org>
2012-11-30 14:30:20 +0000

    Sort members.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/io/trunk@1415656 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/io/input/CharSequenceInputStreamTest.java
+++ src/test/java/org/apache/commons/io/input/CharSequenceInputStreamTest.java
@@ -29,8 +29,10 @@ import org.junit.Test;
 
 public class CharSequenceInputStreamTest {
     
-    private static final String TEST_STRING = "\u00e0 peine arriv\u00e9s nous entr\u00e2mes dans sa chambre";
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String LARGE_TEST_STRING;
+    
+    private static final String TEST_STRING = "\u00e0 peine arriv\u00e9s nous entr\u00e2mes dans sa chambre";
     
     static {
         StringBuilder buffer = new StringBuilder();
@@ -39,23 +41,124 @@ public class CharSequenceInputStreamTest {
         }
         LARGE_TEST_STRING = buffer.toString();
     }
-    
-    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     private Random random = new Random();
     
-    private void testWithSingleByteRead(String testString, String charsetName) throws IOException {
-        byte[] bytes = testString.getBytes(charsetName);
-        InputStream in = new CharSequenceInputStream(testString, charsetName, 512);
-        for (byte b : bytes) {
-            int read = in.read();
-            assertTrue("read "+read+" >=0 ", read >= 0);
-            assertTrue("read "+read+" <= 255", read <= 255);
-            assertEquals("Should agree with input", b, (byte)read);
+    @Test
+    public void testCharsetMismatchInfiniteLoop() throws IOException {
+        // Input is UTF-8 bytes: 0xE0 0xB2 0xA0
+        char[] inputChars = new char[] { (char) 0xE0, (char) 0xB2, (char) 0xA0 };
+        // Charset charset = Charset.forName("UTF-8"); // works
+        Charset charset = Charset.forName("ASCII"); // infinite loop
+        InputStream stream = new CharSequenceInputStream(new String(inputChars), charset, 512);
+        try {
+            while (stream.read() != -1) {
+            }
+        } finally {
+            stream.close();
         }
-        assertEquals(-1, in.read());
     }
     
+    @Test
+    public void testIO_356() throws Exception {
+        testIO_356(10, 10); // OK
+        testIO_356(10, 20); // OK
+        testIO_356(10, 13); // fails
+        for(int bs = 1; bs <= 10; bs++) {
+            for(int ds = 1; ds <= 20; ds++) {
+                testIO_356(bs, ds);                
+            }
+        }
+    }
+    
+    private void testIO_356(int bufferSize, int dataSize) throws Exception {
+        CharSequenceInputStream is = new CharSequenceInputStream(ALPHABET, "UTF-8", bufferSize);
+
+        is.mark(dataSize);
+
+        byte[] data1 = new byte[dataSize];
+        is.read(data1);
+
+        is.reset(); // should allow data to be re-read
+
+        byte[] data2 = new byte[dataSize];
+        is.read(data2);
+
+        is.close();
+        assertArrayEquals("bufferSize="+bufferSize+" dataSize="+dataSize, data1, data2); // data buffers should be identical
+    }
+    
+    @Test
+    public void testLargeUTF8WithBufferedRead() throws IOException {
+        testWithBufferedRead(LARGE_TEST_STRING, "UTF-8");
+    }
+    
+    @Test
+    public void testLargeUTF8WithSingleByteRead() throws IOException {
+        testWithSingleByteRead(LARGE_TEST_STRING, "UTF-8");
+    }
+    
+    @Test
+    public void testMarkReset() throws Exception {
+        InputStream r = new CharSequenceInputStream("test", "UTF-8");
+        r.skip(2);
+        r.mark(0);
+        assertEquals('s', r.read());
+        assertEquals('t', r.read());
+        assertEquals(-1, r.read());
+        r.reset();
+        assertEquals('s', r.read());
+        assertEquals('t', r.read());
+        assertEquals(-1, r.read());
+        r.reset();
+        r.reset();
+    }
+    
+    @Test
+    public void testMarkSupported() throws Exception {
+        InputStream r = new CharSequenceInputStream("test", "UTF-8");
+        assertTrue(r.markSupported());
+    }
+    
+    @Test
+    public void testReadZero() throws Exception {
+        InputStream r = new CharSequenceInputStream("test", "UTF-8");
+        byte[] bytes = new byte[30];
+        assertEquals(0, r.read(bytes, 0, 0));
+    }
+
+    @Test
+    public void testReadZeroEmptyString() throws Exception {
+        InputStream r = new CharSequenceInputStream("", "UTF-8");
+        byte[] bytes = new byte[30];
+        assertEquals(0, r.read(bytes, 0, 0));
+    }
+    
+    @Test
+    public void testSkip() throws Exception {
+        InputStream r = new CharSequenceInputStream("test", "UTF-8");
+        r.skip(1);
+        r.skip(2);
+        assertEquals('t', r.read());
+        r.skip(100);
+        assertEquals(-1, r.read());
+    }
+
+    @Test
+    public void testUTF16WithSingleByteRead() throws IOException {
+        testWithSingleByteRead(TEST_STRING, "UTF-16");
+    }
+
+    @Test
+    public void testUTF8WithBufferedRead() throws IOException {
+        testWithBufferedRead(TEST_STRING, "UTF-8");
+    }
+
+    @Test
+    public void testUTF8WithSingleByteRead() throws IOException {
+        testWithSingleByteRead(TEST_STRING, "UTF-8");
+    }
+
     private void testWithBufferedRead(String testString, String charsetName) throws IOException {
         byte[] expected = testString.getBytes(charsetName);
         InputStream in = new CharSequenceInputStream(testString, charsetName, 512);
@@ -81,118 +184,15 @@ public class CharSequenceInputStreamTest {
         }
     }
     
-    @Test
-    public void testUTF8WithSingleByteRead() throws IOException {
-        testWithSingleByteRead(TEST_STRING, "UTF-8");
-    }
-    
-    @Test
-    public void testLargeUTF8WithSingleByteRead() throws IOException {
-        testWithSingleByteRead(LARGE_TEST_STRING, "UTF-8");
-    }
-    
-    @Test
-    public void testUTF8WithBufferedRead() throws IOException {
-        testWithBufferedRead(TEST_STRING, "UTF-8");
-    }
-    
-    @Test
-    public void testLargeUTF8WithBufferedRead() throws IOException {
-        testWithBufferedRead(LARGE_TEST_STRING, "UTF-8");
-    }
-    
-    @Test
-    public void testUTF16WithSingleByteRead() throws IOException {
-        testWithSingleByteRead(TEST_STRING, "UTF-16");
-    }
-    
-    @Test
-    public void testReadZero() throws Exception {
-        InputStream r = new CharSequenceInputStream("test", "UTF-8");
-        byte[] bytes = new byte[30];
-        assertEquals(0, r.read(bytes, 0, 0));
-    }
-
-    @Test
-    public void testReadZeroEmptyString() throws Exception {
-        InputStream r = new CharSequenceInputStream("", "UTF-8");
-        byte[] bytes = new byte[30];
-        assertEquals(0, r.read(bytes, 0, 0));
-    }
-    
-    @Test
-    public void testCharsetMismatchInfiniteLoop() throws IOException {
-        // Input is UTF-8 bytes: 0xE0 0xB2 0xA0
-        char[] inputChars = new char[] { (char) 0xE0, (char) 0xB2, (char) 0xA0 };
-        // Charset charset = Charset.forName("UTF-8"); // works
-        Charset charset = Charset.forName("ASCII"); // infinite loop
-        InputStream stream = new CharSequenceInputStream(new String(inputChars), charset, 512);
-        try {
-            while (stream.read() != -1) {
-            }
-        } finally {
-            stream.close();
+    private void testWithSingleByteRead(String testString, String charsetName) throws IOException {
+        byte[] bytes = testString.getBytes(charsetName);
+        InputStream in = new CharSequenceInputStream(testString, charsetName, 512);
+        for (byte b : bytes) {
+            int read = in.read();
+            assertTrue("read "+read+" >=0 ", read >= 0);
+            assertTrue("read "+read+" <= 255", read <= 255);
+            assertEquals("Should agree with input", b, (byte)read);
         }
-    }
-
-    @Test
-    public void testSkip() throws Exception {
-        InputStream r = new CharSequenceInputStream("test", "UTF-8");
-        r.skip(1);
-        r.skip(2);
-        assertEquals('t', r.read());
-        r.skip(100);
-        assertEquals(-1, r.read());
-    }
-
-    @Test
-    public void testMarkReset() throws Exception {
-        InputStream r = new CharSequenceInputStream("test", "UTF-8");
-        r.skip(2);
-        r.mark(0);
-        assertEquals('s', r.read());
-        assertEquals('t', r.read());
-        assertEquals(-1, r.read());
-        r.reset();
-        assertEquals('s', r.read());
-        assertEquals('t', r.read());
-        assertEquals(-1, r.read());
-        r.reset();
-        r.reset();
-    }
-
-    @Test
-    public void testMarkSupported() throws Exception {
-        InputStream r = new CharSequenceInputStream("test", "UTF-8");
-        assertTrue(r.markSupported());
-    }
-
-    private void testIO_356(int bufferSize, int dataSize) throws Exception {
-        CharSequenceInputStream is = new CharSequenceInputStream(ALPHABET, "UTF-8", bufferSize);
-
-        is.mark(dataSize);
-
-        byte[] data1 = new byte[dataSize];
-        is.read(data1);
-
-        is.reset(); // should allow data to be re-read
-
-        byte[] data2 = new byte[dataSize];
-        is.read(data2);
-
-        is.close();
-        assertArrayEquals("bufferSize="+bufferSize+" dataSize="+dataSize, data1, data2); // data buffers should be identical
-    }
-    
-    @Test
-    public void testIO_356() throws Exception {
-        testIO_356(10, 10); // OK
-        testIO_356(10, 20); // OK
-        testIO_356(10, 13); // fails
-        for(int bs = 1; bs <= 10; bs++) {
-            for(int ds = 1; ds <= 20; ds++) {
-                testIO_356(bs, ds);                
-            }
-        }
+        assertEquals(-1, in.read());
     }
 }
