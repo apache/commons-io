b469d5353049edde8da7f90e82ced5eb0e92287a
brndnbg <berndjhopp@gmail.com>
2015-02-03 08:57:10 +0100

    reverted import-optimization done by intellij

--- src/main/java/org/apache/commons/io/input/XmlStreamReader.java
+++ src/main/java/org/apache/commons/io/input/XmlStreamReader.java
@@ -16,10 +16,18 @@
  */
 package org.apache.commons.io.input;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
 import org.apache.commons.io.ByteOrderMark;
 import org.apache.commons.io.ThreadLocalByteArray;
 
-import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
