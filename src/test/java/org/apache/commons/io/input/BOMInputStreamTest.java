8fada94f73fc60da897ff48d0b05fb3df43fb59a
Sebastian Bazley <sebb@apache.org>
2015-06-21 14:26:03 +0000

    Unused import
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/io/trunk@1686728 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/io/input/BOMInputStreamTest.java
+++ src/test/java/org/apache/commons/io/input/BOMInputStreamTest.java
@@ -22,7 +22,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
-import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 
 import org.apache.commons.io.ByteOrderMark;
