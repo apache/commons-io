2b9c155e7020908df0d0548571b86ebae34bafe1
Gary D. Gregory <ggregory@apache.org>
2016-05-07 00:07:07 +0000

    [IO-507] Add a ByteOrderFactory class.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/io/trunk@1742676 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/commons/io/ByteOrderFactoryTest.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.io;
+
+import java.nio.ByteOrder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ByteOrderFactoryTest {
+
+    private ByteOrder parseByteOrder(final String value) {
+        return ByteOrderFactory.parseByteOrder(value);
+    }
+
+    @Test
+    public void testParseBig() {
+        Assert.assertEquals(ByteOrder.BIG_ENDIAN, parseByteOrder("big"));
+        Assert.assertEquals(ByteOrder.BIG_ENDIAN, parseByteOrder("Big"));
+        Assert.assertEquals(ByteOrder.BIG_ENDIAN, parseByteOrder("BIG"));
+    }
+
+    @Test
+    public void testParseLittle() {
+        Assert.assertEquals(ByteOrder.LITTLE_ENDIAN, parseByteOrder("little"));
+        Assert.assertEquals(ByteOrder.LITTLE_ENDIAN, parseByteOrder("Little"));
+        Assert.assertEquals(ByteOrder.LITTLE_ENDIAN, parseByteOrder("LITTLE"));
+    }
+}
