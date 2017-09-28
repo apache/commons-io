2341c409ad30a83ea49d95c4b6571ae4479db887
pascalschumacher <pascalschumacher@gmx.net>
2017-09-28 21:03:36 +0200

    IOUtilsCopyTestCase: remove unused import statement

--- src/test/java/org/apache/commons/io/IOUtilsCopyTestCase.java
+++ src/test/java/org/apache/commons/io/IOUtilsCopyTestCase.java
@@ -20,7 +20,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
