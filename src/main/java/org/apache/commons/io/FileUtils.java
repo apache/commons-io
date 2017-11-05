c9fb10f0e45ec66cff342692fd440d06da250e88
Michele Mariotti <mariotti.shape@gmail.com>
2017-11-05 10:32:16 +0100

    [IO-554] fixed: prevent input stream close
    [IO-554] FileUtils.copyToFile(InputStream source, File destination) closes input stream.
    Fixed removing 'source' from try-with-resources.

--- src/main/java/org/apache/commons/io/FileUtils.java
+++ src/main/java/org/apache/commons/io/FileUtils.java
@@ -1525,8 +1525,7 @@ public class FileUtils {
      * @since 2.5
      */
     public static void copyToFile(final InputStream source, final File destination) throws IOException {
-        try (InputStream in = source;
-             OutputStream out = openOutputStream(destination)) {
+        try (OutputStream out = openOutputStream(destination)) {
             IOUtils.copy(in, out);
         }
     }
