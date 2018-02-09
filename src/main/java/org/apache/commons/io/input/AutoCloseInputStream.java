508c6ab21bf6c5ae7c2f028c8a7c651c393928a4
Thomas Mortagne <thomas.mortagne@gmail.com>
2018-02-09 16:31:45 +0100

    IO-568: AutoCloseInputStream should disable mark/reset

--- src/main/java/org/apache/commons/io/input/AutoCloseInputStream.java
+++ src/main/java/org/apache/commons/io/input/AutoCloseInputStream.java
@@ -22,16 +22,20 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * Proxy stream that closes and discards the underlying stream as soon as the end of input has been reached or when the
- * stream is explicitly closed. Not even a reference to the underlying stream is kept after it has been closed, so any
- * allocated in-memory buffers can be freed even if the client application still keeps a reference to the proxy stream.
+ * Proxy stream that closes and discards the underlying stream as soon as the
+ * end of input has been reached or when the stream is explicitly closed.
+ * Not even a reference to the underlying stream is kept after it has been
+ * closed, so any allocated in-memory buffers can be freed even if the
+ * client application still keeps a reference to the proxy stream.
  * <p>
- * This class is typically used to release any resources related to an open stream as soon as possible even if the
- * client application (by not explicitly closing the stream when no longer needed) or the underlying stream (by not
+ * This class is typically used to release any resources related to an open
+ * stream as soon as possible even if the client application (by not explicitly
+ * closing the stream when no longer needed) or the underlying stream (by not
  * releasing resources once the last byte has been read) do not do that.
  * <p>
- * Since the only way to always close the stream when reaching the end and respecting mark/reset contract,
- * AutoCloseInputStream force disabling mark/reset.
+ * Since the only way to always close the stream when reaching the end and
+ * respecting mark/reset contract, AutoCloseInputStream force disabling
+ * mark/reset.
  *
  * @since 1.4
  */
