41059e4a4827e882b7c77667ff62c9b0567e5e21
Gary Gregory <garydgregory@gmail.com>
2018-06-16 15:46:01 -0600

    The the patrol.

--- src/test/java/org/apache/commons/io/input/TailerTest.java
+++ src/test/java/org/apache/commons/io/input/TailerTest.java
@@ -360,7 +360,7 @@ public class TailerTest {
     }
 
     /*
-     * Tests [IO-357][Tailer] InterruptedException while the thead is sleeping is silently ignored.
+     * Tests [IO-357][Tailer] InterruptedException while thead is sleeping is silently ignored.
      */
     @Test
     public void testInterrupt() throws Exception {
