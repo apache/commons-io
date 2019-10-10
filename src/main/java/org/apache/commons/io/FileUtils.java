c06031d17a9563f3086beeb0134d28dbe1cb4f7c
Jerome Hollon <jerome.hollon.contractor@hilton.com>
2019-10-10 15:47:01 -0400

    IO-629: Providing more meaningful exceptions on file delete

--- src/main/java/org/apache/commons/io/FileUtils.java
+++ src/main/java/org/apache/commons/io/FileUtils.java
@@ -34,6 +34,7 @@ import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
