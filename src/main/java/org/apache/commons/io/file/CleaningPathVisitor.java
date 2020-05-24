0fa5a395f5396c9968af30306ce3f6cc89c0d463
Gary Gregory <garydgregory@gmail.com>
2020-05-24 14:04:49 -0400

    Avoid possible NPEs.

--- src/main/java/org/apache/commons/io/file/CleaningPathVisitor.java
+++ src/main/java/org/apache/commons/io/file/CleaningPathVisitor.java
@@ -23,6 +23,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
+import java.util.Objects;
 
 import org.apache.commons.io.file.Counters.PathCounters;
 
@@ -73,7 +74,7 @@ public class CleaningPathVisitor extends CountingPathVisitor {
      * @return true to process the given path, false if not.
      */
     private boolean accept(final Path path) {
-        return Arrays.binarySearch(skip, path.getFileName().toString()) < 0;
+        return Arrays.binarySearch(skip, Objects.toString(path.getFileName(), null)) < 0;
     }
 
     @Override
--- src/main/java/org/apache/commons/io/file/DeletingPathVisitor.java
+++ src/main/java/org/apache/commons/io/file/DeletingPathVisitor.java
@@ -23,6 +23,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
+import java.util.Objects;
 
 import org.apache.commons.io.file.Counters.PathCounters;
 
@@ -75,7 +76,7 @@ public class DeletingPathVisitor extends CountingPathVisitor {
      * @return true to process the given path, false if not.
      */
     private boolean accept(final Path path) {
-        return Arrays.binarySearch(skip, path.getFileName().toString()) < 0;
+        return Arrays.binarySearch(skip, Objects.toString(path.getFileName(), null)) < 0;
     }
 
     @Override
