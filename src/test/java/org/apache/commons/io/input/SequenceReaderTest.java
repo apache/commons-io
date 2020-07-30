b725bb32553bddf55d14ef747773716fcc4ac7d0
Sebb <sebb@apache.org>
2020-07-30 12:42:02 +0100

    Unused

--- src/test/java/org/apache/commons/io/input/SequenceReaderTest.java
+++ src/test/java/org/apache/commons/io/input/SequenceReaderTest.java
@@ -19,7 +19,6 @@ package org.apache.commons.io.input;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.io.Reader;
