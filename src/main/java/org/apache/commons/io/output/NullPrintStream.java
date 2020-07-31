c77c7e79bed1f79df36771d432af6e074ab70f07
Gary Gregory <gardgregory@gmail.com>
2020-07-31 10:35:32 -0400

    Better inline comment.

--- src/main/java/org/apache/commons/io/output/NullPrintStream.java
+++ src/main/java/org/apache/commons/io/output/NullPrintStream.java
@@ -38,7 +38,7 @@ public class NullPrintStream extends PrintStream {
      * Constructs an instance.
      */
     public NullPrintStream() {
-        // Relies on the default charset which is OK since we are not writing.
+        // Relies on the default charset which is OK since we are not actually writing.
         super(NullOutputStream.NULL_OUTPUT_STREAM);
     }
 
