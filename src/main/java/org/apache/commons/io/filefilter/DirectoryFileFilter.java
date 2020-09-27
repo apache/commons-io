57e6135b397ea13ef6628d1906d72c7f5c1b3fb5
Gary Gregory <gardgregory@gmail.com>
2020-09-27 12:21:34 -0400

    Format to line length 120.

--- src/main/java/org/apache/commons/io/filefilter/DirectoryFileFilter.java
+++ src/main/java/org/apache/commons/io/filefilter/DirectoryFileFilter.java
@@ -22,13 +22,12 @@ import java.io.Serializable;
 /**
  * This filter accepts <code>File</code>s that are directories.
  * <p>
- * For example, here is how to print out a list of the
- * current directory's subdirectories:
+ * For example, here is how to print out a list of the current directory's subdirectories:
  *
  * <pre>
  * File dir = new File(".");
- * String[] files = dir.list( DirectoryFileFilter.INSTANCE );
- * for ( int i = 0; i &lt; files.length; i++ ) {
+ * String[] files = dir.list(DirectoryFileFilter.INSTANCE);
+ * for (int i = 0; i &lt; files.length; i++) {
  *     System.out.println(files[i]);
  * }
  * </pre>
@@ -42,28 +41,30 @@ public class DirectoryFileFilter extends AbstractFileFilter implements Serializa
 
     /**
      * Singleton instance of directory filter.
+     * 
      * @since 1.3
      */
     public static final IOFileFilter DIRECTORY = new DirectoryFileFilter();
+
     /**
-     * Singleton instance of directory filter.
-     * Please use the identical DirectoryFileFilter.DIRECTORY constant.
-     * The new name is more JDK 1.5 friendly as it doesn't clash with other
-     * values when using static imports.
+     * Singleton instance of directory filter. Please use the identical DirectoryFileFilter.DIRECTORY constant. The new
+     * name is more JDK 1.5 friendly as it doesn't clash with other values when using static imports.
      */
     public static final IOFileFilter INSTANCE = DIRECTORY;
+
     private static final long serialVersionUID = -5148237843784525732L;
 
     /**
      * Restrictive constructor.
      */
     protected DirectoryFileFilter() {
+        // empty.
     }
 
     /**
      * Checks to see if the file is a directory.
      *
-     * @param file  the File to check
+     * @param file the File to check
      * @return true if the file is a directory
      */
     @Override
