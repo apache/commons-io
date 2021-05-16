c89d9daed4679a5169ce8e2442ddfe78e12ab43b
Gary Gregory <gardgregory@gmail.com>
2021-05-16 08:06:09 -0400

    Sort members.

--- src/test/java/org/apache/commons/io/function/IOFunctionTest.java
+++ src/test/java/org/apache/commons/io/function/IOFunctionTest.java
@@ -31,81 +31,24 @@ import org.junit.jupiter.api.Test;
 
 public class IOFunctionTest {
 
-    @Test
-    public void testApply() throws IOException {
-        final IOFunction<InputStream, Integer> readByte = InputStream::read;
-        final InputStream is = new ByteArrayInputStream(new byte[] { (byte)0xa, (byte)0xb, (byte)0xc});
-        assertEquals(0xa, readByte.apply(is));
-        assertEquals(0xb, readByte.apply(is));
-        assertEquals(0xc, readByte.apply(is));
-        assertEquals(-1, readByte.apply(is));
+    private static class Holder<T> {
+        T value;
     }
 
     @Test
-    public void testApplyRaisesException() {
-        final IOFunction<InputStream, Integer> raiseException = is -> {
-            throw new IOException("Boom!");
+    public void testAndThenConsumer() throws IOException {
+        final Holder<Integer> holder = new Holder<>();
+        final IOFunction<InputStream, Integer> readByte = InputStream::read;
+        final Consumer<Integer> sinkInteger = i -> {
+            holder.value = i * i;
         };
-        final InputStream is = new ByteArrayInputStream(new byte[] { (byte)0xa, (byte)0xb, (byte)0xc});
-
-        assertThrows(IOException.class, () -> {
-            raiseException.apply(is);
-        });
-    }
-
-    @Test
-    public void testComposeIOFunction() throws IOException {
-        final IOFunction<InputStream, Integer> readByte = InputStream::read;
-        final IOFunction<Integer, Integer> squareInteger = i -> i * i;
-        final IOFunction<InputStream, Integer> productFunction = squareInteger.compose(readByte);
+        final IOConsumer<InputStream> productFunction = readByte.andThen(sinkInteger);
 
         final InputStream is = new ByteArrayInputStream(new byte[] {2, 3});
-        assertEquals(4, productFunction.apply(is));
-        assertEquals(9, productFunction.apply(is));
-    }
-
-    @Test
-    public void testComposeFunction() throws IOException {
-        final Function<InputStream, Integer> alwaysSeven = is -> 7;
-        final IOFunction<Integer, Integer> squareInteger = i -> i * i;
-        final IOFunction<InputStream, Integer> productFunction = squareInteger.compose(alwaysSeven);
-
-        final InputStream is = new ByteArrayInputStream(new byte[] {2, 3});
-        assertEquals(49, productFunction.apply(is));
-        assertEquals(49, productFunction.apply(is));
-    }
-
-    @Test
-    public void testComposeIOSupplier() throws IOException {
-        final InputStream is = new ByteArrayInputStream(new byte[] {2, 3});
-
-        final IOSupplier<Integer> readByte = () -> is.read();
-        final IOFunction<Integer, Integer> squareInteger = i -> i * i;
-        final IOSupplier<Integer> productFunction = squareInteger.compose(readByte);
-
-        assertEquals(4, productFunction.get());
-        assertEquals(9, productFunction.get());
-    }
-
-    @Test
-    public void testComposeSupplier() throws IOException {
-        final Supplier<Integer> alwaysNine = () -> 9;
-        final IOFunction<Integer, Integer> squareInteger = i -> i * i;
-        final IOSupplier<Integer> productFunction = squareInteger.compose(alwaysNine);
-
-        assertEquals(81, productFunction.get());
-        assertEquals(81, productFunction.get());
-    }
-
-    @Test
-    public void testAndThenIOFunction() throws IOException {
-        final IOFunction<InputStream, Integer> readByte = InputStream::read;
-        final IOFunction<Integer, Integer> squareInteger = i -> i * i;
-        final IOFunction<InputStream, Integer> productFunction = readByte.andThen(squareInteger);
-
-        final InputStream is = new ByteArrayInputStream(new byte[] {2, 3});
-        assertEquals(4, productFunction.apply(is));
-        assertEquals(9, productFunction.apply(is));
+        productFunction.accept(is);
+        assertEquals(4, holder.value);
+        productFunction.accept(is);
+        assertEquals(9, holder.value);
     }
 
     @Test
@@ -136,19 +79,80 @@ public class IOFunctionTest {
     }
 
     @Test
-    public void testAndThenConsumer() throws IOException {
-        final Holder<Integer> holder = new Holder<>();
+    public void testAndThenIOFunction() throws IOException {
         final IOFunction<InputStream, Integer> readByte = InputStream::read;
-        final Consumer<Integer> sinkInteger = i -> {
-            holder.value = i * i;
-        };
-        final IOConsumer<InputStream> productFunction = readByte.andThen(sinkInteger);
+        final IOFunction<Integer, Integer> squareInteger = i -> i * i;
+        final IOFunction<InputStream, Integer> productFunction = readByte.andThen(squareInteger);
 
         final InputStream is = new ByteArrayInputStream(new byte[] {2, 3});
-        productFunction.accept(is);
-        assertEquals(4, holder.value);
-        productFunction.accept(is);
-        assertEquals(9, holder.value);
+        assertEquals(4, productFunction.apply(is));
+        assertEquals(9, productFunction.apply(is));
+    }
+
+    @Test
+    public void testApply() throws IOException {
+        final IOFunction<InputStream, Integer> readByte = InputStream::read;
+        final InputStream is = new ByteArrayInputStream(new byte[] { (byte)0xa, (byte)0xb, (byte)0xc});
+        assertEquals(0xa, readByte.apply(is));
+        assertEquals(0xb, readByte.apply(is));
+        assertEquals(0xc, readByte.apply(is));
+        assertEquals(-1, readByte.apply(is));
+    }
+
+    @Test
+    public void testApplyRaisesException() {
+        final IOFunction<InputStream, Integer> raiseException = is -> {
+            throw new IOException("Boom!");
+        };
+        final InputStream is = new ByteArrayInputStream(new byte[] { (byte)0xa, (byte)0xb, (byte)0xc});
+
+        assertThrows(IOException.class, () -> {
+            raiseException.apply(is);
+        });
+    }
+
+    @Test
+    public void testComposeFunction() throws IOException {
+        final Function<InputStream, Integer> alwaysSeven = is -> 7;
+        final IOFunction<Integer, Integer> squareInteger = i -> i * i;
+        final IOFunction<InputStream, Integer> productFunction = squareInteger.compose(alwaysSeven);
+
+        final InputStream is = new ByteArrayInputStream(new byte[] {2, 3});
+        assertEquals(49, productFunction.apply(is));
+        assertEquals(49, productFunction.apply(is));
+    }
+
+    @Test
+    public void testComposeIOFunction() throws IOException {
+        final IOFunction<InputStream, Integer> readByte = InputStream::read;
+        final IOFunction<Integer, Integer> squareInteger = i -> i * i;
+        final IOFunction<InputStream, Integer> productFunction = squareInteger.compose(readByte);
+
+        final InputStream is = new ByteArrayInputStream(new byte[] {2, 3});
+        assertEquals(4, productFunction.apply(is));
+        assertEquals(9, productFunction.apply(is));
+    }
+
+    @Test
+    public void testComposeIOSupplier() throws IOException {
+        final InputStream is = new ByteArrayInputStream(new byte[] {2, 3});
+
+        final IOSupplier<Integer> readByte = () -> is.read();
+        final IOFunction<Integer, Integer> squareInteger = i -> i * i;
+        final IOSupplier<Integer> productFunction = squareInteger.compose(readByte);
+
+        assertEquals(4, productFunction.get());
+        assertEquals(9, productFunction.get());
+    }
+
+    @Test
+    public void testComposeSupplier() throws IOException {
+        final Supplier<Integer> alwaysNine = () -> 9;
+        final IOFunction<Integer, Integer> squareInteger = i -> i * i;
+        final IOSupplier<Integer> productFunction = squareInteger.compose(alwaysNine);
+
+        assertEquals(81, productFunction.get());
+        assertEquals(81, productFunction.get());
     }
 
     @Test
@@ -157,9 +161,5 @@ public class IOFunctionTest {
         try (final InputStream is = new ByteArrayInputStream(new byte[] { (byte) 0xa, (byte) 0xb, (byte) 0xc })) {
             assertEquals(is, identityFunction.apply(is));
         }
-    }
-
-    private static class Holder<T> {
-        T value;
     }
 }
