d8c1aa12c61fa0fb0a015139d129edabcdd3518b
Gary Gregory <gardgregory@gmail.com>
2021-05-16 08:11:23 -0400

    Move license header to the top of the file.

--- src/test/java/org/apache/commons/io/output/ThresholdingOutputStreamTest.java
+++ src/test/java/org/apache/commons/io/output/ThresholdingOutputStreamTest.java
@@ -1,4 +1,3 @@
-package org.apache.commons.io.output;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -15,6 +14,9 @@ package org.apache.commons.io.output;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.commons.io.output;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
