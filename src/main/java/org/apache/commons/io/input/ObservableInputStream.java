cd2f42d8f3768c817dd01c74d1bd07da8038b9c0
Gary Gregory <gardgregory@gmail.com>
2021-05-19 11:10:03 -0400

    Javadoc.

--- src/main/java/org/apache/commons/io/input/ObservableInputStream.java
+++ src/main/java/org/apache/commons/io/input/ObservableInputStream.java
@@ -28,10 +28,15 @@ import org.apache.commons.io.IOUtils;
 
 /**
  * The {@link ObservableInputStream} allows, that an InputStream may be consumed by other receivers, apart from the
- * thread, which is reading it. The other consumers are implemented as instances of {@link Observer}. A typical
- * application may be the generation of a {@link java.security.MessageDigest} on the fly. {@code Note}: The
- * {@link ObservableInputStream} is <em>not</em> thread safe, as instances of InputStream usually aren't. If you must
- * access the stream from multiple threads, then synchronization, locking, or a similar means must be used.
+ * thread, which is reading it. The other consumers are implemented as instances of {@link Observer}.
+ * <p>
+ * A typical application may be the generation of a {@link java.security.MessageDigest} on the fly.
+ * </p>
+ * <p>
+ * <em>Note</em>: The {@link ObservableInputStream} is <em>not</em> thread safe, as instances of InputStream usually
+ * aren't. If you must access the stream from multiple threads, then synchronization, locking, or a similar means must
+ * be used.
+ * </p>
  *
  * @see MessageDigestCalculatingInputStream
  */
