c2b40f218c8cb33ee91a92df07b1c2556b38601e
Richard Adams <richard@researchspace.com>
2022-03-25 19:23:10 +0000

    escape <

--- src/main/java/org/apache/commons/io/IOUtils.java
+++ src/main/java/org/apache/commons/io/IOUtils.java
@@ -2500,7 +2500,7 @@ public class IOUtils {
      * {@code toByteArray(InputStream)} when {@code InputStream} size is known.
      *
      * @param input the {@code InputStream} to read.
-     * @param size the size of {@code InputStream} to read, where 0 < {@code size} <= length of input stream.
+     * @param size the size of {@code InputStream} to read, where 0 &lt; {@code size} &lt;= length of input stream.
      * @return byte [] of length {@size}.
      * @throws IOException if an I/O error occurs or {@code InputStream} length is smaller than parameter {@code size}.
      * @throws IllegalArgumentException if {@code size} is less than zero.
@@ -2540,7 +2540,7 @@ public class IOUtils {
      * (Arrays can have no more than Integer.MAX_VALUE entries anyway)
      *
      * @param input the {@code InputStream} to read from
-     * @param size the size of {@code InputStream} to read, where 0 < {@code size} <= min(Integer.MAX_VALUE, length of input stream).
+     * @param size the size of {@code InputStream} to read, where 0 &lt; {@code size} &lt;= min(Integer.MAX_VALUE, length of input stream).
      * @return byte [] the requested byte array, of length {@code size}
      * @throws IOException              if an I/O error occurs or {@code InputStream} length is less than {@code size}
      * @throws IllegalArgumentException if size is less than zero or size is greater than Integer.MAX_VALUE
