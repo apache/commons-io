3a15a75b58f451cd30d7a685d586a29300e0a28d
Gary Gregory <garydgregory@gmail.com>
2022-06-09 10:04:20 -0400

    Use try-with-resources

--- src/test/java/org/apache/commons/io/input/BOMInputStreamTest.java
+++ src/test/java/org/apache/commons/io/input/BOMInputStreamTest.java
@@ -192,16 +192,17 @@ public class BOMInputStreamTest {
     }
 
     private void readBOMInputStreamTwice(final String resource) throws Exception {
-        final InputStream inputStream = this.getClass().getResourceAsStream(resource);
-        assertNotNull(inputStream);
-        final BOMInputStream bomInputStream = new BOMInputStream(inputStream);
-        bomInputStream.mark(1000000);
+        try (final InputStream inputStream = this.getClass().getResourceAsStream(resource)) {
+            assertNotNull(inputStream);
+            try (final BOMInputStream bomInputStream = new BOMInputStream(inputStream)) {
+                bomInputStream.mark(1000000);
 
-        this.readFile(bomInputStream);
-        bomInputStream.reset();
-        this.readFile(bomInputStream);
-        inputStream.close();
-        bomInputStream.close();
+                this.readFile(bomInputStream);
+                bomInputStream.reset();
+                this.readFile(bomInputStream);
+                inputStream.close();
+            }
+        }
     }
 
     private void readFile(final BOMInputStream bomInputStream) throws Exception {
