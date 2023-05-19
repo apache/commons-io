4e8284a67b7b184c5da9e237178258eb4ee9273f
Gary Gregory <garydgregory@gmail.com>
2023-05-19 10:38:46 -0400

    Javadoc

--- src/main/java/org/apache/commons/io/output/UncheckedFilterOutputStream.java
+++ src/main/java/org/apache/commons/io/output/UncheckedFilterOutputStream.java
@@ -21,7 +21,9 @@ import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UncheckedIOException;
+import java.nio.file.OpenOption;
 
+import org.apache.commons.io.build.AbstractOrigin;
 import org.apache.commons.io.build.AbstractStreamBuilder;
 import org.apache.commons.io.function.Uncheck;
 
@@ -61,8 +63,17 @@ public final class UncheckedFilterOutputStream extends FilterOutputStream {
 
         /**
          * Constructs a new instance.
+         * <p>
+         * This builder use the aspect OutputStream.
+         * </p>
+         * <p>
+         * You must provide an origin that can be converted to an OutputStream by this builder, otherwise, this call will throw an
+         * {@link UnsupportedOperationException}.
+         * </p>
          *
-         * @throws UnsupportedOperationException if the origin cannot be converted to an OutputStream.
+         * @return a new instance.
+         * @throws UnsupportedOperationException if the origin cannot provide an OutputStream.
+         * @see AbstractOrigin#getOutputStream(OpenOption...)
          */
         @SuppressWarnings("resource")
         @Override
