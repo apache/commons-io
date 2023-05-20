6d29faf9ed6c0eac1fa1aadd9ec71a8eb4bdf0c6
Gary Gregory <garydgregory@gmail.com>
2023-05-20 10:53:06 -0400

    Format tweak

--- src/main/java/org/apache/commons/io/output/ChunkedOutputStream.java
+++ src/main/java/org/apache/commons/io/output/ChunkedOutputStream.java
@@ -73,7 +73,7 @@ public class ChunkedOutputStream extends FilterOutputStream {
     public void write(final byte[] data, final int srcOffset, final int length) throws IOException {
         int bytes = length;
         int dstOffset = srcOffset;
-        while(bytes > 0) {
+        while (bytes > 0) {
             final int chunk = Math.min(bytes, chunkSize);
             out.write(data, dstOffset, chunk);
             bytes -= chunk;
