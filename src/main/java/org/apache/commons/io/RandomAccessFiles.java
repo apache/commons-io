dd93554b692cabe884e7a097e431f475397a3b48
Gary Gregory <garydgregory@gmail.com>
2023-10-08 09:08:28 -0400

    Improve performance of PathUtils.fileContentEquals(Path, Path)
    - Add org.apache.commons.io.channels.FileChannels.
    - Add RandomAccessFiles#contentEquals(RandomAccessFile,
    RandomAccessFile).
    - Add RandomAccessFiles#reset(RandomAccessFile).
    - Add PathUtilsContentEqualsBenchmark.

--- src/main/java/org/apache/commons/io/RandomAccessFiles.java
+++ src/main/java/org/apache/commons/io/RandomAccessFiles.java
@@ -19,13 +19,53 @@ package org.apache.commons.io;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.util.Objects;
+
+import org.apache.commons.io.channels.FileChannels;
 
 /**
- * Works on RandomAccessFile.
+ * Works with {@link RandomAccessFile}.
  *
  * @since 2.13.0
  */
 public class RandomAccessFiles {
+
+    /**
+     * Tests if two RandomAccessFile contents are equal.
+     *
+     * @param raf1 A RandomAccessFile.
+     * @param raf2 Another RandomAccessFile.
+     * @return true if the contents of both RandomAccessFiles are equal, false otherwise.
+     * @throws IOException if an I/O error occurs.
+     * @since 2.15.0
+     */
+    @SuppressWarnings("resource") // See comments
+    public static boolean contentEquals(final RandomAccessFile raf1, final RandomAccessFile raf2) throws IOException {
+        // Short-circuit test
+        if (Objects.equals(raf1, raf2)) {
+            return true;
+        }
+        // Short-circuit test
+        final long length1 = length(raf1);
+        final long length2 = length(raf2);
+        if (length1 != length2) {
+            return false;
+        }
+        if (length1 == 0 && length2 == 0) {
+            return true;
+        }
+        // Dig in and to the work
+        // We do not close FileChannels because that closes the owning RandomAccessFile.
+        // Instead, the caller is assumed to manage the given RandomAccessFile objects.
+        final FileChannel channel1 = raf1.getChannel();
+        final FileChannel channel2 = raf2.getChannel();
+        return FileChannels.contentEquals(channel1, channel2, IOUtils.DEFAULT_BUFFER_SIZE);
+    }
+
+    private static long length(final RandomAccessFile raf) throws IOException {
+        return raf != null ? raf.length() : 0;
+    }
 
     /**
      * Reads a byte array starting at "position" for "length" bytes.
@@ -40,6 +80,19 @@ public class RandomAccessFiles {
     public static byte[] read(final RandomAccessFile input, final long position, final int length) throws IOException {
         input.seek(position);
         return IOUtils.toByteArray(input::read, length);
+    }
+
+    /**
+     * Resets the given file to position 0.
+     *
+     * @param raf The RandomAccessFile to reset.
+     * @return The given RandomAccessFile.
+     * @throws IOException If {@code pos} is less than {@code 0} or if an I/O error occurs.
+     * @since 2.15.0
+     */
+    public static RandomAccessFile reset(final RandomAccessFile raf) throws IOException {
+        raf.seek(0);
+        return raf;
     }
 
 }
