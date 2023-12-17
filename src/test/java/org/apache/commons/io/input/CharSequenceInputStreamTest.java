1350f159b5850b1f42c8dd0a331f07e7d955bd05
Elliotte Rusty Harold <elharo@ibiblio.org>
2023-12-17 15:30:05 -0500

    detab

--- src/test/java/org/apache/commons/io/input/CharSequenceInputStreamTest.java
+++ src/test/java/org/apache/commons/io/input/CharSequenceInputStreamTest.java
@@ -516,10 +516,10 @@ public class CharSequenceInputStreamTest {
     // IO-781 available() returns 2 but only 1 byte is read afterwards
     public void testAvailable() throws IOException {
         final Charset charset = Charset.forName("Big5");
-    	final CharSequenceInputStream in = new CharSequenceInputStream("\uD800\uDC00", charset);
-    	final int available = in.available();
-    	final byte[] data = new byte[available];
-    	final int bytesRead = in.read(data);
-    	assertEquals(available, bytesRead);
+        final CharSequenceInputStream in = new CharSequenceInputStream("\uD800\uDC00", charset);
+        final int available = in.available();
+        final byte[] data = new byte[available];
+        final int bytesRead = in.read(data);
+        assertEquals(available, bytesRead);
     }
 }
