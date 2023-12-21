035e048708b8f64fda49810289a8f9fcdd3dd116
Gary Gregory <garydgregory@gmail.com>
2023-12-21 17:09:34 -0500

    Javadoc spelling

--- src/main/java/org/apache/commons/io/input/SwappedDataInputStream.java
+++ src/main/java/org/apache/commons/io/input/SwappedDataInputStream.java
@@ -27,7 +27,7 @@ import org.apache.commons.io.EndianUtils;
 
 /**
  * DataInput for systems relying on little-endian data formats. When read, values will be changed from little-endian to
- * big endian formats for internal usage.
+ * big-endian formats for internal usage.
  * <p>
  * Provenance: Avalon Excalibur (IO)
  * </p>
