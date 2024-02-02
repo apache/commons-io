b1b27902bc9a9f6a7eeed75e03f4a495bbd077d6
Elliotte Rusty Harold <elharo@ibiblio.org>
2024-02-02 07:55:07 -0500

    add test for subdirectory link repointing

--- src/test/java/org/apache/commons/io/FileUtilsTest.java
+++ src/test/java/org/apache/commons/io/FileUtilsTest.java
@@ -819,6 +819,41 @@ public class FileUtilsTest extends AbstractTempDirTest {
         assertEquals(copiedContent.toPath(), source);
     }
 
+    @Test
+    public void testCopyDirectory_symLinkInternalFile_nested() throws Exception {
+        // Make a directory
+        final File realDirectory = new File(tempDirFile, "real_directory");
+        final File subDirectoryA = new File(realDirectory, "A");
+        final File subDirectoryB = new File(realDirectory, "B");
+        subDirectoryB.mkdirs();
+
+        // make a file
+        final File content = new File(subDirectoryA, "hello.txt");
+        FileUtils.writeStringToFile(content, "HELLO WORLD", "UTF8");
+
+        // Make a symlink to the file
+        final Path linkPath = subDirectoryB.toPath().resolve("link_to_file");
+        Files.createSymbolicLink(linkPath, content.toPath());
+
+        // Now copy the directory
+        final File destination = new File(tempDirFile, "destination");
+        FileUtils.copyDirectory(realDirectory, destination);
+
+        // delete the original
+        content.delete();
+
+        // test that the copied directory contains a link to the copied file
+        final File copiedLink = new File(new File(destination, "B"), "link_to_file");
+        assertTrue(Files.isSymbolicLink(copiedLink.toPath()));
+        final String actual = FileUtils.readFileToString(copiedLink, "UTF8");
+        assertEquals("HELLO WORLD", actual);
+
+        final Path source = Files.readSymbolicLink(copiedLink.toPath());
+        assertNotEquals(content.toPath(), source);
+        final File copiedContent = new File(new File(destination, "A"), "hello.txt");
+        assertEquals(copiedContent.toPath(), source);
+    }
+
     /**
      * See what happens when copyDirectory copies a directory that is a symlink
      * to another directory containing non-symlinked files.
