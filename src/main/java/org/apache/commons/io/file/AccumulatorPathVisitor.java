5d2737ffe489b91c4af7ccddfeda93d860750729
Gary D. Gregory <garydgregory@gmail.com>
2025-07-10 11:16:05 -0400

    Add @SuppressWarnings

--- src/main/java/org/apache/commons/io/file/AccumulatorPathVisitor.java
+++ src/main/java/org/apache/commons/io/file/AccumulatorPathVisitor.java
@@ -195,6 +195,7 @@ public class AccumulatorPathVisitor extends CountingPathVisitor {
      * @since 2.12.0
      * @deprecated Use {@link #builder()}.
      */
+    @SuppressWarnings("deprecation")
     @Deprecated
     public AccumulatorPathVisitor(final PathCounters pathCounter, final PathFilter fileFilter, final PathFilter dirFilter,
         final IOBiFunction<Path, IOException, FileVisitResult> visitFileFailed) {
