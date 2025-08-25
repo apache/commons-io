774841f3dee4d650387e3c33db8899042ba26ede
Gary D. Gregory <garydgregory@gmail.com>
2025-08-25 17:07:55 -0400

    Javadoc

--- src/test/java/org/apache/commons/io/output/ByteArrayOutputStreamTest.java
+++ src/test/java/org/apache/commons/io/output/ByteArrayOutputStreamTest.java
@@ -43,7 +43,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 /**
- * Tests the alternative ByteArrayOutputStream implementations.
+ * Tests the alternative {@link ByteArrayOutputStream} implementations.
  */
 class ByteArrayOutputStreamTest {
 
