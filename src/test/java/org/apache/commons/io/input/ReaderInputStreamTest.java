6df255f1271501d447bdbf89e62a339c9c50915a
Md Tanvir Alam <tanviralamshuvo@gmail.com>
2026-08-27 18:40:22 +0000

    [IO-780] Remove unused assertDoesNotThrow import

--- src/test/java/org/apache/commons/io/input/ReaderInputStreamTest.java
+++ src/test/java/org/apache/commons/io/input/ReaderInputStreamTest.java
@@ -16,7 +16,6 @@
  */
 package org.apache.commons.io.input;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
